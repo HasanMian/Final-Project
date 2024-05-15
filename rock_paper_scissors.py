b9bda3ff903b3b8593792d4f67c077e3d8e1d38b
HasanMian <hasan_aslam@icloud.com>
2024-05-15 19:26:52 -0400

    Update #2 rock_paper_scissors.py
    This section of the code is designed for the dice to roll.

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -7,4 +7,12 @@ def roll_dice(num_dice):
   return total
 
 def number_of_rolls(player1_choice, player2_choice):
-    if player1_choice = player2_choice
+    if player1_choice == player2_choice
+        return 1,1 # rolls dice
+elif (player 1_choice == 'rock' and player2_choice == 'paper') or \
+    (player1_choice == 'paper' and player2_choice == 'scissors') or \
+    (player1_choice == 'scissors' and player2_choice == 'rock'):
+    return 2, 1 # Player 1 rolls twice, Player 2 rolls once
+else: 
+    return 1, 2 # Player 1
+
