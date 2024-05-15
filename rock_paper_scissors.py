1ee3b644cea4cac8f16baf74eb1e92e6f32edb04
HasanMian <hasan_aslam@icloud.com>
2024-05-14 23:05:45 -0400

    Create rock_paper_scissors.py
    I created the first portion of the code. This is a vital part of the code because the range defines the number of rolls.

---
+++ rock_paper_scissors.py
@@ -0,0 +1,10 @@
+import random
+
+def roll_dice(num_dice):
+  total = 0
+  for in range(num_dice):
+    total =+ random.randint(1, 6)
+  return total
+
+def number_of_rolls(player1_choice, player2_choice):
+    if player1_choice = player2_choice
