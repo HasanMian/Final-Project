73b3a7eff10c1002553d1978e930abb734b55b61
HasanMian <hasan_aslam@icloud.com>
2024-05-15 22:34:20 -0400

    Update rock_paper_scissors.py
    Entire code is completed. Now need to run the program to see if it works efficiently.

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -19,34 +19,55 @@ else:
 def get_choice(player):
     choice = ''
     while choice not in ['rock', 'paper', 'scissors']:
-        choice = input(f"{player}, pick your choice (rock, paper, or scissors): ".lower()
+        choice = input(f"{player}, pick your choice (rock, paper, or scissors): ".lower() # Each Player can select there choice of either rock, paper, or scissors
     return choice
 
-def play_round():
+def play_game():
     print("\nNew Round: ")
-    player1_choice = get_choice('Player 1')
+    player1_choice = get_choice('Player 1') 
     player2_choice = get_choice('Player 2')
 
-    print(f" Player 1 chose {player1_choice}")
+    print(f" Player 1 chose {player1_choice}") 
     print(f" Player 2 chose {player2_choice}")
 
-    player1_dice, player2_dice = number_of_rolls(player1_choice, player2_choice)
+    player1_dice, player2_dice = number_of_rolls(player1_choice, player2_choice) 
 
     player1_roll = roll_dice(player1_dice)
     player2_roll = roll_dice(player2_dice)
 
-    print(f" Player 1 rolls {player1_dice} dice and gets a score of {player1_roll}")
+    print(f" Player 1 rolls {player1_dice} dice and gets a score of {player1_roll}") 
     print(f" Player 2 rolls {player2_dice} dice and gets a score of {player2_roll}")
 
     if player1_roll > player2_roll:
-        print("Player 1 wins this round!")
+        print("Player 1 wins this game!") # If player ones roll is higher than player two, player one wins
         return 1, 0
     elif player2_roll > player1_roll:
-        print("Player 2 wins this round!")
-        return 1, 0
+        print("Player 2 wins this game!") # If player two rolls a higher number than he wins
+        return 0, 1
     else:
-        print("This round ended with a tie!")
+        print("This game ended with a tie!")
         return 0, 0 
 
 def play_game(): 
+    print("Welcome to the new way to play Rock, Paper, Scissors!")
+    games = int(input("Enter the number of rounds to play: ")
+
+    player1_score = 0
+    player2_score = 0
+
+    for in range(again):
+        player1, player2 = play_again()
+        player1_score += player1
+        player2_score += player2
+        print (f"Scores: Player 1: {player1_score}, Player 2: {player2_score}") # This outputs but players score
+
+    if player1_score > player2_score:
+        print("Player 1 wins the game!") # If player ones score is higher than they win the game
+elif player2_score > player1_score:
+    print("Player 2 wins the game!" # If player two's score is higher than they win the game
+  else:
+      print("The game is a tie!")
+
+if __name__ == "__main__":
+    play_game()
     
