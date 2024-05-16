1e653c5f083c4f1309599e361874d691ddcdd60e
HasanMian <hasan_aslam@icloud.com>
2024-05-15 20:56:42 -0400

    Update rock_paper_scissors.py
    Developed the round part of the code. I was experiencing a syntax error due to not having a bracket in a certain spot.

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -16,3 +16,37 @@ elif (player 1_choice == 'rock' and player2_choice == 'paper') or \
 else: 
     return 1, 2 # Player 1
 
+def get_choice(player):
+    choice = ''
+    while choice not in ['rock', 'paper', 'scissors']:
+        choice = input(f"{player}, pick your choice (rock, paper, or scissors): ".lower()
+    return choice
+
+def play_round():
+    print("\nNew Round: ")
+    player1_choice = get_choice('Player 1')
+    player2_choice = get_choice('Player 2')
+
+    print(f" Player 1 chose {player1_choice}")
+    print(f" Player 2 chose {player2_choice}")
+
+    player1_dice, player2_dice = number_of_rolls(player1_choice, player2_choice)
+
+    player1_roll = roll_dice(player1_dice)
+    player2_roll = roll_dice(player2_dice)
+
+    print(f" Player 1 rolls {player1_dice} dice and gets a score of {player1_roll}")
+    print(f" Player 2 rolls {player2_dice} dice and gets a score of {player2_roll}")
+
+    if player1_roll > player2_roll:
+        print("Player 1 wins this round!")
+        return 1, 0
+    elif player2_roll > player1_roll:
+        print("Player 2 wins this round!")
+        return 1, 0
+    else:
+        print("This round ended with a tie!")
+        return 0, 0 
+
+def play_game(): 
+    
